d7ba765bfe0a180bfa99f71dbfdc039aefd6e7bc
Akos Becsey <becsey.a@gmail.com>
2024-10-04 14:23:49 +0200

    dynamic data

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,59 @@
 #include <avr/wdt.h>
 #include <util/delay.h>
 
-#include "arduino-mcp2515/mcp2515.h"
+#include "can_data.hpp"
+#include "fastnet.hpp"
+#include "model.hpp"
 #include "systick.hpp"
-#include "usart.h"
 
-char D_120_DEG_3_KT_B[] = "\xFF\x70\x0C\x01\x84\x4E\x88\x01\x74\x51\x01\x00\x78\x52\x08\x55\x50\x96";
+static constexpr uint32_t EMISSION_PERIOD_MS = 1000;
+static constexpr uint32_t PHASE_SHIFT        = 50;
 
-struct can_frame canMsg;
-MCP2515          mcp2515;
+uint32_t phase_of_emission = 0;
+uint32_t p_last_emission   = 0;
+
+// can be used for collision detection
+// void packet_emission_shift_phase() {
+//     auto new_phase = phase_of_emission;
+//     new_phase += PHASE_SHIFT;
+//
+//     if (new_phase > EMISSION_PERIOD_MS) {
+//         p_last_emission++;
+//         new_phase -= EMISSION_PERIOD_MS;
+//     }
+//
+//     phase_of_emission = new_phase;
+// }
+
+void packet_emission_update() {
+    uint32_t now    = systick_get_ms();
+    uint16_t period = now / EMISSION_PERIOD_MS;
+
+    bool model_was_updated = model.evaluateUpdateFlag();
+    bool period_has_passed = period > p_last_emission;
+    bool phase_is_matching = now % EMISSION_PERIOD_MS >= phase_of_emission;
+
+    if (model_was_updated && period_has_passed && phase_is_matching) {
+        sendWindData();
+        // TODO led::WindwiserData::toggle();
+
+        p_last_emission = period;
+    }
+}
 
 int main() {
 
     systick_init();
     USART_Init(10560);
+    can_data_init();
 
     set_sleep_mode(SLEEP_MODE_IDLE);
     wdt_enable(WDTO_2S);
     sei();
 
-    mcp2515.reset();
-    mcp2515.setBitrate(CAN_125KBPS);
-    mcp2515.setNormalMode();
-
     while (1) {
-        if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
-            USART_SendBlocking(D_120_DEG_3_KT_B, sizeof(D_120_DEG_3_KT_B) - 1);
+        can_data_update();
+        packet_emission_update();
 
         wdt_reset();
     }
